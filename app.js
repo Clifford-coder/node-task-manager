401b41754ab5b391ce60985df5a737bc5261194d
Clifford-coder <cliffowusu2727@gmail.com>
2021-10-14 16:29:05 -0700

    Connected server to mongodb using mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 const app = express();
 
@@ -8,10 +10,6 @@ const app = express();
 app.use(express.json()); //to be able to get the date in req.body
 
 //routes
-app.get('/hell', (req, res) => {
-  res.send('<h1>Hell</h1>');
-});
-
 app.use('/api/v1/tasks', tasks);
 //app.get('api/v1/tasks') - get all task
 //app.post('api/v1/tasks') - create new task.
@@ -21,4 +19,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is running on port ${port} ...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port} ...`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,12 @@
 //connect to the mongodb using mongoose
+const mongoose = require('mongoose');
+
+const connectDB = (url) =>
+  mongoose.connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+module.exports = connectDB;
